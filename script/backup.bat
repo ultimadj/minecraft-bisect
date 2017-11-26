4e8aee37d5f3529e3653439511947dca3b0dea5c
David Johnson <ultimadj@gmail.com>
2017-11-26 12:38:15 -0500

    Call secrets file

--- script/backup.bat
+++ script/backup.bat
@@ -1,1 +1,11 @@
-echo Test!
+set SCRIPT_DIR=%~dp0
+@ECHO OFF
+call  %SCRIPT_DIR%/../tmp/secrets.bat
+IF "%SECRET_BISECT_USER%"=="" (
+    echo Missing secrets file... see readme.md
+    GOTO END
+)
+@ECHO ON 
+set s
+
+:END
