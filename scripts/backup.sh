dba5d9b3fbb42ffa6fff1029e29f73f37b8783ae
David Johnson <ultimadj@gmail.com>
2018-01-14 14:38:40 -0500

    Uncomment actual backup

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -7,4 +7,4 @@ BACKUP_DIR=/d/tmp/minecraft-bisect/backup/$(date '+%Y%m%d_%k%M')/
 mkdir -p $BACKUP_DIR
 # https://stackoverflow.com/questions/113886/how-to-recursively-download-a-folder-via-ftp-on-linux
 cd $BACKUP_DIR
-#wget -m --user=$FTP_USER --password=$FTP_PASSWORD ftp://$FTP_HOST 
+wget -m --user=$FTP_USER --password=$FTP_PASSWORD ftp://$FTP_HOST 
